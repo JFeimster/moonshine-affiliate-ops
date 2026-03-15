2c5ff544a7baeeed14b0499ac935e5f70177b962
Jason Feimster <jasonfeimster@gmail.com>
2026-03-15 05:28:49 -0400

    Update uiStore.ts

--- store/uiStore.ts
+++ store/uiStore.ts
@@ -4,15 +4,16 @@ import { create } from "zustand"
 
 type DrawerType = "createCampaign" | "editCampaign" | "linkQR" | null
 type ModalType = "archive" | null
+type Payload = Record<string, string> | null
 
 interface UIState {
   drawer: DrawerType
-  drawerPayload: Record<string, string> | null
+  drawerPayload: Payload
   modal: ModalType
-  modalPayload: Record<string, string> | null
-  openDrawer: (drawer: DrawerType, payload?: Record<string, string>) => void
+  modalPayload: Payload
+  openDrawer: (drawer: DrawerType, payload?: Payload) => void
   closeDrawer: () => void
-  openModal: (modal: ModalType, payload?: Record<string, string>) => void
+  openModal: (modal: ModalType, payload?: Payload) => void
   closeModal: () => void
 }
 
@@ -21,8 +22,28 @@ export const useUIStore = create<UIState>((set) => ({
   drawerPayload: null,
   modal: null,
   modalPayload: null,
-  openDrawer: (drawer, payload = null) => set({ drawer, drawerPayload: payload }),
-  closeDrawer: () => set({ drawer: null, drawerPayload: null }),
-  openModal: (modal, payload = null) => set({ modal, modalPayload: payload }),
-  closeModal: () => set({ modal: null, modalPayload: null }),
+
+  openDrawer: (drawer, payload = null) =>
+    set({
+      drawer,
+      drawerPayload: payload,
+    }),
+
+  closeDrawer: () =>
+    set({
+      drawer: null,
+      drawerPayload: null,
+    }),
+
+  openModal: (modal, payload = null) =>
+    set({
+      modal,
+      modalPayload: payload,
+    }),
+
+  closeModal: () =>
+    set({
+      modal: null,
+      modalPayload: null,
+    }),
 }))
